239827eabdf9f8255e13608723bedb7b39a0532e
azimut <azimut.github@protonmail.com>
2019-09-12 04:50:37 -0300

    not really a double as time is not involved on interpolation

--- src/queries.lisp
+++ src/queries.lisp
@@ -1,26 +1,29 @@
 (in-package #:rocketman)
 
+(declaim (inline interpolate))
+(defun interpolate (first second row)
+  (let ((dt (/ (- row (key-row first))
+               (- (key-row second) (key-row first))))
+        (dv (- (key-value second)
+               (key-value first))))
+    (declare (type single-float dv))
+    (ecase (key-interpolation first)
+      (0 (key-value first))
+      (1 (+ (key-value first) (* dv dt)))
+      (2 (+ (key-value first) (* dv (* dt dt (- 3 (* 2 dt))))))
+      (3 (+ (key-value first) (* dv (expt dt 2)))))))
+
 (defmethod get-track :around ((obj rocket) (track-id string))
+  "Converts track name to id AND ensure zero if track is missing"
   (let ((id (gethash track-id (state-name2id obj))))
     (if id
         (get-track obj id)
         0f0)))
 
-(declaim (inline interpolate))
-(defun interpolate (first second row)
-  (let ((dt (/ (- row (key-row first))
-               (- (key-row second) (key-row first))))
-        (dv (- (key-value second) (key-value first))))
-    (ecase (key-interpolation first)
-      (0 (key-value first))
-      (1 (+ (key-value first) (* dv dt)))
-      (2 (+ (key-value first) (* dv (* dt dt (- 3 (* 2 dt))))))
-      (3 (+ (key-value first) (* dv (expt dt 2f0)))))))
-
 (defmethod get-track ((obj rocket) track-id)
-  (let* ((track (aref (state-tracks obj) track-id))
+  (let* ((track    (aref (state-tracks obj) track-id))
          (n-tracks (length track))
-         (row (state-row obj)))
+         (row      (state-row obj)))
     (cond ((= 0 n-tracks)
            0f0)
           ((< row (key-row (first track)))
@@ -30,7 +33,7 @@
           (t (let ((top-pos (position row track :test #'< :key #'key-row)))
                (if top-pos
                    (interpolate (nth (1- top-pos) track)
-                                (nth top-pos      track)
+                                (nth     top-pos  track)
                                 row)
                    (key-value (a:lastcar track))))))))
 
